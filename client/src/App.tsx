de6df324816fcf1d5b91c49f22761bf2addf5036
AXIS-Platform-Poland <ingvarrsp.z.o.o@gmail.com>
2026-02-08 00:30:52 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import HomePage from "./pages/HomePage";
-import FriendsPage from "./pages/FriendsPage";
-import MarketplacePage from "./pages/MarketplacePage";
-import ReelsPage from "./pages/ReelsPage";
-import SettingsPage from "./pages/SettingsPage";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Topbar from "./components/Topbar";
 
+import HomePage from "./pages/HomePage";
+import FriendsPage from "./pages/FriendsPage";
+import ReelsPage from "./pages/ReelsPage";
+import MarketplacePage from "./pages/MarketplacePage";
+import SettingsPage from "./pages/SettingsPage";
+
 export default function App() {
   return (
-    <Router>
-      <div className="flex w-full h-screen">
+    <BrowserRouter>
+      <div style={{ display: "flex", minHeight: "100vh" }}>
         <Sidebar />
-
-        <div className="flex-1 flex flex-col">
+        <div style={{ flex: 1 }}>
           <Topbar />
-
-          <div className="p-4 overflow-auto">
+          <div style={{ padding: 16 }}>
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/friends" element={<FriendsPage />} />
               <Route path="/reels" element={<ReelsPage />} />
               <Route path="/marketplace" element={<MarketplacePage />} />
               <Route path="/settings" element={<SettingsPage />} />
+
+              {/* запасной маршрут */}
+              <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </div>
         </div>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
