634b3aa715a8653467a39a3a8bcc235a7df024be
AXIS-Platform-Poland <ingvarrsp.z.o.o@gmail.com>
2026-02-13 01:53:41 +0100

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+
 import { AuthProvider } from "./AuthContext";
+import { I18nProvider } from "./i18n/I18nProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <I18nProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </I18nProvider>
   </React.StrictMode>
 );
