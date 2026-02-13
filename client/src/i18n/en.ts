8773720f3a6d7b492502469017825ca6c6783908
AXIS-Platform-Poland <ingvarrsp.z.o.o@gmail.com>
2026-02-13 01:48:40 +0100

    Create en.ts

---
+++ client/src/i18n/en.ts
@@ -0,0 +1,15 @@
+export const en = {
+  auth: {
+    title: "Sign up",
+    email: "Email",
+    password: "Password",
+    create: "Create account",
+    haveAccount: "Already have an account? Sign in",
+    errorConfirmEmail: "Error sending confirmation email",
+  },
+  common: {
+    language: "Language",
+    save: "Save",
+    signOut: "Sign out",
+  },
+} as const;
