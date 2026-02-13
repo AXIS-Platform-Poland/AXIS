c63e0fa5de74b4b61023b182a47433d476c8ba60
AXIS-Platform-Poland <ingvarrsp.z.o.o@gmail.com>
2026-02-13 02:12:21 +0100

    Create pl.ts

---
+++ client/src/i18n/pl.ts
@@ -0,0 +1,11 @@
+const pl = {
+  posts: "Posty",
+  friends: "Znajomi",
+  reels: "Rolki",
+  marketplace: "Marketplace",
+  settings: "Ustawienia",
+  login: "Zaloguj się",
+  register: "Rejestracja",
+};
+
+export default pl;
