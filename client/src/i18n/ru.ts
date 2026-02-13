c4d9a5fed7fc5a13931204d154a88f795f13c0f3
AXIS-Platform-Poland <ingvarrsp.z.o.o@gmail.com>
2026-02-13 01:47:52 +0100

    Create ru.ts

---
+++ client/src/i18n/ru.ts
@@ -0,0 +1,15 @@
+export const ru = {
+  auth: {
+    title: "Регистрация",
+    email: "Email",
+    password: "Пароль",
+    create: "Создать аккаунт",
+    haveAccount: "Уже есть аккаунт? Войти",
+    errorConfirmEmail: "Ошибка отправки письма подтверждения",
+  },
+  common: {
+    language: "Язык",
+    save: "Сохранить",
+    signOut: "Выйти",
+  },
+} as const;
